1e7648c64010cd319e9d6aac31f7f623ec95a6b5
Roger Torres <torresroger445@gmail.com>
2022-05-30 16:53:41 -0500

    feat: :tada: First configuration to run this project

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons';
+
+const App = () => {
+  return (
+    <>
+      <Text>Hello World</Text>
+      <Icon name="star-outline" size={100} />
+    </>
+  );
+};
+
+export default App;
